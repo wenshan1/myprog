ece66f5cf1cd720bdada7939d32c53e70ae6d939
Bin Lan <bin.lan@windriver.com>
2017-01-10 19:47:11 +0800

    删除不要的模板文件

--- src/main/java/me/wenshan/web/MyTimer.java
+++ src/main/java/me/wenshan/web/MyTimer.java
@@ -14,7 +14,7 @@ public class MyTimer {
     @Autowired
     private OptionManager opm;
     
-    @Scheduled(cron = "0 0/2 * * * ?")
+    @Scheduled(cron = "0 0 0/1 * * ?")
     public void updateHourly_sc () {
         UpdateHourlyThread.updateHourly (smmManager, opm, null);
     }
